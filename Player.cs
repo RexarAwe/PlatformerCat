9e3315e5a1db00586bc994f213f5f2a553d87b98
Gavin Kijkul <gavinkijkul@gmail.com>
2021-02-06 13:05:21 +0700

    player wall jump and animation

--- Player.cs
+++ Player.cs
@@ -16,9 +16,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpVelocity;
     [SerializeField] private float jumpVelocityCut;
 
+    private bool wallSliding = false;
+    [SerializeField] private float wallSlidingSpeed;
+    private bool wallJumping;
+    [SerializeField] private float xWallForce;
+    [SerializeField] private float yWallForce;
+    [SerializeField] private float wallJumpTime;
 
     [SerializeField] private LayerMask platformLayer; 
     private BoxCollider2D boxcoll2d;
+
+    private Animator animator;
 
     private GameManager gmScript;
 
@@ -27,6 +35,7 @@ public class Player : MonoBehaviour
     {
         playerRB = GetComponent<Rigidbody2D>();
         boxcoll2d = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
 
         gmScript = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
@@ -36,7 +45,13 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         //transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
-        playerRB.velocity = new Vector2(horizontalInput * speed, playerRB.velocity.y);
+        
+        if(IsGrounded())
+        {
+            playerRB.velocity = new Vector2(horizontalInput * speed, playerRB.velocity.y);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput * speed));
 
         jumpDelay -= Time.deltaTime;
         jumpDelay2 -= Time.deltaTime;
@@ -51,6 +66,8 @@ public class Player : MonoBehaviour
         {
             jumpDelay2 = jumpDelayTimer2;
         }
+
+        
 
         if (jumpDelay > 0 && jumpDelay2 > 0)
         {
@@ -68,6 +85,55 @@ public class Player : MonoBehaviour
                 playerRB.velocity = new Vector2(playerRB.velocity.x, playerRB.velocity.y * jumpVelocityCut);
             }
         }
+
+        if (nearLeftWall() && !IsGrounded() && (Input.GetKey("left") || Input.GetKey("a")))
+        {
+            wallSliding = true;
+            //StartCoroutine("TopRightJump");
+            //playerRB.AddForce(new Vector2(10, 10), ForceMode2D.Impulse);
+            //playerRB.velocity = new Vector2(20, jumpVelocity);
+        }
+
+        if (nearRightWall() && !IsGrounded() && (Input.GetKey("right") || Input.GetKey("d")))
+        {
+            wallSliding = true;
+            //playerRB.velocity = new Vector2(-20, jumpVelocity);
+            //playerRB.AddForce(transform.up * 30, ForceMode2D.Impulse);
+            //playerRB.AddForce(transform.right * -30, ForceMode2D.Impulse);
+        }
+
+        if (wallSliding)
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, Mathf.Clamp(playerRB.velocity.y, -wallSlidingSpeed, float.MaxValue));
+
+            // wall jump
+            if(Input.GetKeyDown("space"))
+            {
+                wallJumping = true;
+                Invoke("notWallJumping", wallJumpTime);
+            }
+
+        }
+
+        if (wallJumping)
+        {
+            if(nearRightWall())
+                playerRB.velocity = new Vector2(-xWallForce, yWallForce);
+            else if(nearLeftWall())
+                playerRB.velocity = new Vector2(xWallForce, yWallForce);
+
+            //playerRB.AddForce(new Vector2(-xWallForce, yWallForce), ForceMode2D.Force);
+        }
+
+        if (!nearLeftWall() || !nearRightWall() || IsGrounded())
+        {
+            wallSliding = false;
+        }
+    }
+
+    private void notWallJumping()
+    {
+        wallJumping = false;
     }
 
     private bool IsGrounded()
@@ -115,5 +181,38 @@ public class Player : MonoBehaviour
             //Destroy(col.gameObject);
             Destroy(gameObject);
         }
+    }
+
+
+    private bool nearLeftWall()
+    {
+        float extraWidth = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(boxcoll2d.bounds.center - new Vector3(boxcoll2d.bounds.extents.x, 0), Vector3.left, extraWidth, platformLayer);
+
+        Color rayColor;
+        if (raycastHit.collider != null)
+            rayColor = Color.green;
+        else
+            rayColor = Color.red;
+
+        Debug.DrawRay(boxcoll2d.bounds.center - new Vector3(boxcoll2d.bounds.extents.x, 0), new Vector3(-extraWidth, 0), rayColor);
+
+        return raycastHit.collider != null;
+    }
+
+    private bool nearRightWall()
+    {
+        float extraWidth = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(boxcoll2d.bounds.center + new Vector3(boxcoll2d.bounds.extents.x, 0), Vector3.right, extraWidth, platformLayer);
+
+        Color rayColor;
+        if (raycastHit.collider != null)
+            rayColor = Color.green;
+        else
+            rayColor = Color.red;
+
+        Debug.DrawRay(boxcoll2d.bounds.center + new Vector3(boxcoll2d.bounds.extents.x, 0), new Vector3(extraWidth, 0), rayColor);
+
+        return raycastHit.collider != null;
     }
 }
